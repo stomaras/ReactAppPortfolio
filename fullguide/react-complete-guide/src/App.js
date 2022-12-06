e2eb5ed3808abdfb415445eb21262ca48f77be5e
stomaras <sptoma724@gmail.com>
2022-12-06 22:29:38 +0200

    Understanding Keys

--- fullguide/react-complete-guide/src/App.js
+++ fullguide/react-complete-guide/src/App.js
@@ -2,32 +2,42 @@ import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import React from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 296.23,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e2",
+    title: "Dog food",
+    amount: 29.23,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e3",
+    title: "Toilet Paper",
+    amount: 96.23,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Coffeee",
+    amount: 80.23,
+    date: new Date(2022, 2, 28),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 296.23,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e2",
-      title: "Dog food",
-      amount: 29.23,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e3",
-      title: "Toilet Paper",
-      amount: 96.23,
-      date: new Date(2021, 2, 28),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   // return React.createElement(
   //   "div",
