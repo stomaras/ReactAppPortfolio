b4312b050093599c94430566ff2f1bd01ce2de8e
stomaras <sptoma724@gmail.com>
2022-12-07 20:04:45 +0200

    Section 5 done

--- fullguide/react-complete-guide/src/components/Expenses/Expenses.js
+++ fullguide/react-complete-guide/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2021");
@@ -25,6 +26,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
